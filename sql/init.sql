b639f40fa9d7e8b2e6f6ac5986080285529e827f
Dmitrii Rudik <dima.rudik@gmail.com>
2024-03-11 10:03:10 +0300

    README

---
+++ sql/init.sql
@@ -0,0 +1,40 @@
+create role test with login superuser password 'test';
+create table table1 (
+    id bigint,
+    create_at timestamp(6) with time zone,
+    name character varying(255),
+    update_at timestamp(6) with time zone,
+    gender boolean,
+    byteablob bytea,
+    textclob text
+);
+--create table source (
+--    id int primary key generated always as identity,
+--    uuid uuid,
+--    name varchar(256),
+--    boolean boolean,
+--    int2 int2,
+--    int4 int4,
+--    int8 int8,
+--    smallint smallint,
+--    bigint bigint,
+--    numeric numeric,
+--    float8 float8,
+--    date date,
+--    timestamp timestamp,
+--    timestamptz timestamptz,
+--    description text,
+--    image bytea
+--);
+--create table target as select * from source where 0 = 1;
+--insert into source(uuid, name, boolean,
+--        int2, int4, int8, smallint, bigint, numeric, float8,
+--        date, timestamp, timestamptz, description, image)
+--    select gen_random_uuid() uuid, 'PostgreSQL ' || n name, case when mod(n, 2) = 0 then false else true end boolean,
+--        0 as int2, n as int4, n as int8, 10 as smallint, n as bigint, n / pi() as numeric, n / pi() as float8,
+--        current_date, current_timestamp, current_timestamp,
+--        rpad('PostgreSQL', 1000, '*') description,
+--        case when mod(n, 1000) = 0 then pg_read_binary_file('/var/lib/postgresql/DimaExa1_cutted_400_505.jpg')::bytea
+--        end image
+--    from generate_series(1,100000) as n;
+--vacuum source;
