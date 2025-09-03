5473acccbeb17a5bed15e7255c33fc8ef2d7692a
Dima Rudik <dima.rudik@gmail.com>
2025-09-03 11:53:00 +0300

    CSPool

--- bublik-postgres/src/main/java/org/bublik/postgres/storage/JDBCPostgreSQLStorage.java
+++ bublik-postgres/src/main/java/org/bublik/postgres/storage/JDBCPostgreSQLStorage.java
@@ -906,12 +906,12 @@ public class JDBCPostgreSQLStorage extends JDBCStorage implements JDBCStorageSer
                                 row.setText(targetColumn, s.replaceAll("\u0000", ""));
                                 break;
                             } else {
-                                log.error("\u001B[31mThere is no handler for type : {}\u001B[0m", targetType);
+                                log.error("There is no handler for type: {} for column: {}", targetType, targetColumn);
                                 writer.close();
                                 connectionTo.close();
                             }
                         } else {
-                            log.error("\u001B[31mtryCharIfAny is NULL for type: {}\u001B[0m", targetType);
+                            log.error("tryCharIfAny is NULL for type: {} for column: {}", targetType, targetColumn);
                             throw new RuntimeException();
                         }
                     } catch (BinaryWriteFailedException | SQLException e) {
