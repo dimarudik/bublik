533904a831d13f2867b46589fa05211c57524f2c
dimarudik <dima.rudik@gmail.com>
2025-09-29 17:41:02 +0300

    multi-architecture

--- bublik-cli/src/test/java/org/bublik/cli/PgToPgEnvSwitchoverTest.java
+++ bublik-cli/src/test/java/org/bublik/cli/PgToPgEnvSwitchoverTest.java
@@ -42,16 +42,52 @@ public class PgToPgEnvSwitchoverTest {
             .withCommand("etcd --name etcd3 --initial-advertise-peer-urls http://etcd3:2380")
             .withNetwork(network);
 
+    private static GenericContainer<?> patroni1 = new GenericContainer<>("dimarudik/patroni")
+            .withEnv("PATRONI_RESTAPI_USERNAME", "admin")
+            .withEnv("PATRONI_RESTAPI_PASSWORD", "admin")
+            .withEnv("PATRONI_SUPERUSER_USERNAME", "postgres")
+            .withEnv("PATRONI_SUPERUSER_PASSWORD", "postgres")
+            .withEnv("PATRONI_REPLICATION_USERNAME", "replicator")
+            .withEnv("PATRONI_REPLICATION_PASSWORD", "replicate")
+            .withEnv("PATRONI_admin_PASSWORD", "admin")
+            .withEnv("PATRONI_admin_OPTIONS", "createdb,createrole")
+            .withEnv("ETCD_ENDPOINTS", "http://etcd1:2379,http://etcd2:2379,http://etcd3:2379")
+            .withEnv("PATRONI_ETCD3_HOSTS", "'etcd1:2379','etcd2:2379','etcd3:2379'")
+            .withEnv("PATRONI_SCOPE", "demo")
+            .withEnv("PATRONI_NAME", "patroni1")
+            .withCreateContainerCmdModifier(cmd -> cmd.withHostName("patroni1"))
+            .withNetwork(network);
+
+    private static GenericContainer<?> patroni2 = new GenericContainer<>("dimarudik/patroni")
+            .withEnv("PATRONI_RESTAPI_USERNAME", "admin")
+            .withEnv("PATRONI_RESTAPI_PASSWORD", "admin")
+            .withEnv("PATRONI_SUPERUSER_USERNAME", "postgres")
+            .withEnv("PATRONI_SUPERUSER_PASSWORD", "postgres")
+            .withEnv("PATRONI_REPLICATION_USERNAME", "replicator")
+            .withEnv("PATRONI_REPLICATION_PASSWORD", "replicate")
+            .withEnv("PATRONI_admin_PASSWORD", "admin")
+            .withEnv("PATRONI_admin_OPTIONS", "createdb,createrole")
+            .withEnv("ETCD_ENDPOINTS", "http://etcd1:2379,http://etcd2:2379,http://etcd3:2379")
+            .withEnv("PATRONI_ETCD3_HOSTS", "'etcd1:2379','etcd2:2379','etcd3:2379'")
+            .withEnv("PATRONI_SCOPE", "demo")
+            .withEnv("PATRONI_NAME", "patroni2")
+            .withCreateContainerCmdModifier(cmd -> cmd.withHostName("patroni2"))
+            .withNetwork(network);
+
     @BeforeAll
     static void setUp() {
         etcd1.start();
         etcd2.start();
         etcd3.start();
+        patroni1.setPortBindings(java.util.Collections.singletonList("5432:5432"));
+        patroni2.setPortBindings(java.util.Collections.singletonList("5433:5432"));
+        patroni1.start();
+        patroni2.start();
     }
 
     @Test
     public void init() throws InterruptedException {
-        Thread.sleep(120_000);
+//        Thread.sleep(120_000);
         assertEquals(0, 0);
     }
 }
