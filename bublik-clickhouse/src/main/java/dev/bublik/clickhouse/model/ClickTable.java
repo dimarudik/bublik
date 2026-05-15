1616e81efb3d9296bc66e138887af389102be313
dimarudik <dima.rudik@gmail.com>
2026-05-15 12:34:28 +0300

    v26.2.35

---
+++ bublik-clickhouse/src/main/java/dev/bublik/clickhouse/model/ClickTable.java
@@ -0,0 +1,114 @@
+package dev.bublik.clickhouse.model;
+
+import dev.bublik.core.model.*;
+import dev.bublik.core.storage.Storage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class ClickTable <S extends Connection> extends Table<S> {
+    private static final Logger log = LoggerFactory.getLogger(ClickTable.class);
+
+    public ClickTable(String schemaName, String tableName) {
+        super(schemaName, tableName);
+    }
+
+    @Override
+    public boolean exists(Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public String getFinalTableName(boolean withQuotes) {
+        return "";
+    }
+
+    @Override
+    public String getFinalSchemaName() {
+        return "";
+    }
+
+    @Override
+    public String getHintClause() {
+        return "";
+    }
+
+    @Override
+    public List<Column> getAllColumns(S connection) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Column> getPrimaryKeyColumns(Connection connection) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<ForeignKey> getForeignKeys(Connection connection, Storage storage, Table targetTable) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Index> getTableIndexes(Connection connection) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<UniqueConstraint> getUniqueConstraints(Connection connection) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Map.Entry<Integer, List<TableOption>> getOptions(Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean hasPrimaryKey() {
+        return false;
+    }
+
+    @Override
+    public void createPrimaryKey(Connection connection) {
+
+    }
+
+    @Override
+    public void createIndexes(Connection connection) {
+
+    }
+
+    @Override
+    public void createUniqueConstraints(Connection connection) {
+
+    }
+
+    @Override
+    public void createForeignKeys(Connection connection) {
+
+    }
+
+    @Override
+    public Map<String, String> getColumnToColumn(Connection connection) throws SQLException {
+        return Map.of();
+    }
+
+    @Override
+    public void create(Connection connection) throws SQLException {
+
+    }
+
+    @Override
+    public boolean enrichTable(S session) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public String buildOrderBy(Config config) {
+        return "";
+    }
+}
