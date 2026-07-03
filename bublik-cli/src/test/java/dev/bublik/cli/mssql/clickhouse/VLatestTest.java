d02d38d8f8bee412715dbff8d65bad0519756915
dimarudik <dima.rudik@gmail.com>
2026-07-03 10:29:37 +0300

    v26.3.1

--- bublik-cli/src/test/java/dev/bublik/cli/mssql/clickhouse/VLatestTest.java
+++ bublik-cli/src/test/java/dev/bublik/cli/mssql/clickhouse/VLatestTest.java
@@ -21,14 +21,14 @@ public class VLatestTest {
     private static int rows = 20000;
     private static final JdbcDatabaseContainer<?> source = new MSSQLServerContainer("mcr.microsoft.com/mssql/server")
             .acceptLicense()
-            .withInitScript("./mssql/clickhouse/sql/mssql.sql");
+            .withInitScript("mssql/clickhouse/sql/mssql.sql");
 
     private static final DockerImageName CLICKHOUSE_LATEST = DockerImageName
             .parse("clickhouse")
             .asCompatibleSubstituteFor("clickhouse/clickhouse-server");
 
     private static final ClickHouseContainer target = new ClickHouseContainer(CLICKHOUSE_LATEST)
-            .withInitScript("./mssql/clickhouse/sql/click.sql");
+            .withInitScript("mssql/clickhouse/sql/click.sql");
 
     @BeforeAll
     static void setUp() {
